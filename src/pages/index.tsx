08288e977f4bc2881e0a2df90932a836b751633c
Callum Wellwood-Kane <cwellkane@gmail.com>
2022-07-24 14:46:27 +1000

    feat(places-sdk): convert the google service into a hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,22 @@
 import type { NextPage } from "next";
 import React, { useState } from "react";
 import { AddressSearch } from "../components/AddressSearch";
-import { PlaceTypes } from "../../types/placeTypes";
 import { FormState } from "../../types/form-types";
 
 const Home: NextPage = () => {
-  const [placeObject, setPlaceObject] = useState<PlaceTypes>({
-    latitude: 0,
-    longitude: 0,
-    html_attributions: [],
-    place_id: "",
-  });
+  const [placeObject, setPlaceObject] = useState<string>("");
 
   const [formState, setFormState] = useState<FormState>({
+    id: "",
     address: "",
     radius: "",
     keywords: "",
   });
 
-  console.debug(formState)
+  console.debug(placeObject, formState);
   return (
     <>
-      <AddressSearch
-        formState={formState}
-        setFormState={setFormState}
-        setPlaceObject={setPlaceObject}
-      />
+      <AddressSearch formState={formState} setFormState={setFormState} />
       {/* <DisplayRestaurant placeObject={placeObject} /> */}
     </>
   );
